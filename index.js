9c02142124b4b02c03acf5a01727662435635ec6
altafziyaa <altafziyaa91@gmail.com>
2026-02-06 02:25:41 +0530

    Fix Express setup for Vercel (remove listen, export app)

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
+// import express from "express";
+// import connectDB from "./src/config/db.js";
+// import authRoutes from "./src/routes/auth.routes.js";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const app = express();
+// const PORT = process.env.PORT || 5001;
+// app.use(
+//   cors({
+//     origin: "*", // ya gateway URL
+//   }),
+// );
+
+// app.use(express.json());
+// app.get("/", (req, res) => {
+//   res.send("Auth Service is running 🚀");
+// });
+
+// app.use("/api/auth", authRoutes);
+
+// connectDB();
+
+// app.listen(PORT, () => console.log(`Auth Service Running on port ${PORT}`));
+
 import express from "express";
 import connectDB from "./src/config/db.js";
 import authRoutes from "./src/routes/auth.routes.js";
 import cors from "cors";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
-app.use(
-  cors({
-    origin: "*", // ya gateway URL
-  }),
-);
 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Auth Service is running 🚀");
 });
@@ -22,4 +45,4 @@ app.use("/api/auth", authRoutes);
 
 connectDB();
 
-app.listen(PORT, () => console.log(`Auth Service Running on port ${PORT}`));
+export default app; // ✅ Vercel requires this
