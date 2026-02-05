28791343298eeba16627abfd44e6979d430341eb
altafziyaa <altafziyaa91@gmail.com>
2026-02-06 01:56:28 +0530

    Resolve merge conflict

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import connectDB from "./src/config/db.js";
 import authRoutes from "./src/routes/auth.routes.js";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+app.use(
+  cors({
+    origin: "*", // ya gateway URL
+  }),
+);
 
 app.use(express.json());
 app.get("/", (req, res) => {
