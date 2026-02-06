214f57bfbf1768fa9b4e904fefa1c0f4199811f2
altafziyaa <altafziyaa91@gmail.com>
2026-02-06 16:42:33 +0530

    update

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-// import express from "express";
-// import connectDB from "./src/config/db.js";
-// import authRoutes from "./src/routes/auth.routes.js";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// const app = express();
-// const PORT = process.env.PORT || 5001;
-// app.use(
-//   cors({
-//     origin: "*", // ya gateway URL
-//   }),
-// );
-
-// app.use(express.json());
-// app.get("/", (req, res) => {
-//   res.send("Auth Service is running 🚀");
-// });
-
-// app.use("/api/auth", authRoutes);
-
-// connectDB();
-
-// app.listen(PORT, () => console.log(`Auth Service Running on port ${PORT}`));
-
 import express from "express";
 import connectDB from "./src/config/db.js";
 import authRoutes from "./src/routes/auth.routes.js";
@@ -45,4 +19,4 @@ app.use("/api/auth", authRoutes);
 
 connectDB();
 
-export default app; // ✅ Vercel requires this
+export default app;
