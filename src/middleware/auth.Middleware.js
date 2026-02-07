6fde433d8671ef9d5c799e8f9a348412aec1999d
altafziyaa <altafziyaa91@gmail.com>
2026-02-07 16:58:40 +0530

    debugging

--- src/middleware/auth.Middleware.js
+++ src/middleware/auth.Middleware.js
@@ -2,24 +2,26 @@ import jwt from "jsonwebtoken";
 import { jwtConfig } from "../config/jwt.js";
 
 export const authMiddleware = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  console.log("AUTH HEADER:", req.headers.authorization);
-
   try {
+    const authHeader = req.headers.authorization;
+
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res
-        .status(401)
-        .json({ success: false, message: "Unauthorized : token missing" });
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized : token missing",
+      });
     }
+
     const token = authHeader.split(" ")[1];
 
-    const tokenDecode = jwt.verify(token, jwtConfig.accessSecret);
+    const decoded = jwt.verify(token, jwtConfig.accessSecret);
 
     req.user = {
-      userId: tokenDecode.userId,
-      role: tokenDecode.role,
+      userId: decoded.userId,
+      role: decoded.role,
     };
-    return next();
+
+    next();
   } catch (error) {
     return res.status(401).json({
       success: false,
